ff4ecd0a4eb9682ddb0080613183f53054fc6ec8
Nisarga Adhikary <adhikarynisarga17@gmail.com>
2021-10-14 21:15:38 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,9 @@ import { Telegraf } from "telegraf";
 if (!process.env.TELEGRAM_BOT_TOKEN) throw new Error("Please add a bot token");
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
-bot.start(ctx => ctx.reply("Welcome"));
+bot.start(ctx => ctx.reply("Welcome! Waaasuup?"));
 bot.hears("hello", ctx => {
-  ctx.reply("Hello to you too!");
+  ctx.reply("Hello to you too! My friend!!");
 });
 
 bot.launch();
